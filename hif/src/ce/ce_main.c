OnePlusOSS/android_kernel_oneplus_msm8998

qcacmn: hif: Replace instances of unadorned %p
Replace instances of unadorned %p in hif.

Change-Id: I282573452a2c12b4bccdbdaab611dcd6f84a6240
CRs-Fixed: 2111274

hif/src/ce/ce_main.c
@@ -1541,7 +1541,7 @@ static inline void hif_ce_do_recv(struct hif_msg_callbacks *msg_callbacks,
 			rxCompletionHandler(msg_callbacks->Context,
 					netbuf, pipe_info->pipe_num);
 	} else {
-		HIF_ERROR("%s: Invalid Rx msg buf:%p nbytes:%d",
+		HIF_ERROR("%s: Invalid Rx msg buf:%pK nbytes:%d",
 				__func__, netbuf, nbytes);
 
 		qdf_nbuf_free(netbuf);
@@ -1647,7 +1647,7 @@ static int hif_completion_thread_startup(struct HIF_CE_state *hif_state)
 		attr = host_ce_config[pipe_num];
 		if (attr.src_nentries) {
 			/* pipe used to send to target */
-			HIF_DBG("%s: pipe_num:%d pipe_info:0x%p",
+			HIF_DBG("%s: pipe_num:%d pipe_info:0x%pK",
 					 __func__, pipe_num, pipe_info);
 			ce_send_cb_register(pipe_info->ce_hdl,
 					    hif_pci_ce_send_done, pipe_info,

hif/src/hif_napi.c
@@ -181,17 +181,17 @@ int hif_napi_create(struct hif_opaque_softc   *hif_ctx,
 
 		init_dummy_netdev(&(napii->netdev));
 
-		NAPI_DEBUG("adding napi=%p to netdev=%p (poll=%p, bdgt=%d)",
+		NAPI_DEBUG("adding napi=%pK to netdev=%pK (poll=%pK, bdgt=%d)",
 			   &(napii->napi), &(napii->netdev), poll, budget);
 		netif_napi_add(&(napii->netdev), &(napii->napi), poll, budget);
 
 		NAPI_DEBUG("after napi_add");
-		NAPI_DEBUG("napi=0x%p, netdev=0x%p",
+		NAPI_DEBUG("napi=0x%pK, netdev=0x%pK",
 			   &(napii->napi), &(napii->netdev));
-		NAPI_DEBUG("napi.dev_list.prev=0x%p, next=0x%p",
+		NAPI_DEBUG("napi.dev_list.prev=0x%pK, next=0x%pK",
 			   napii->napi.dev_list.prev,
 			   napii->napi.dev_list.next);
-		NAPI_DEBUG("dev.napi_list.prev=0x%p, next=0x%p",
+		NAPI_DEBUG("dev.napi_list.prev=0x%pK, next=0x%pK",
 			   napii->netdev.napi_list.prev,
 			   napii->netdev.napi_list.next);
 
@@ -287,10 +287,10 @@ int hif_napi_destroy(struct hif_opaque_softc *hif_ctx,
 		}
 		if (0 == rc) {
 			NAPI_DEBUG("before napi_del");
-			NAPI_DEBUG("napi.dlist.prv=0x%p, next=0x%p",
+			NAPI_DEBUG("napi.dlist.prv=0x%pK, next=0x%pK",
 				  napii->napi.dev_list.prev,
 				  napii->napi.dev_list.next);
-			NAPI_DEBUG("dev.napi_l.prv=0x%p, next=0x%p",
+			NAPI_DEBUG("dev.napi_l.prv=0x%pK, next=0x%pK",
 				   napii->netdev.napi_list.prev,
 				   napii->netdev.napi_list.next);
 
@@ -356,7 +356,7 @@ int hif_napi_lro_flush_cb_register(struct hif_opaque_softc *hif_hdl,
 				}
 				napii->lro_flush_cb = lro_flush_handler;
 				napii->lro_ctx = data;
-				HIF_DBG("Registering LRO for ce_id %d NAPI callback for %d flush_cb %p, lro_data %p\n",
+				HIF_DBG("Registering LRO for ce_id %d NAPI callback for %d flush_cb %pK, lro_data %pK\n",
 					i, napii->id, napii->lro_flush_cb,
 					napii->lro_ctx);
 				rc++;
@@ -398,7 +398,7 @@ void hif_napi_lro_flush_cb_deregister(struct hif_opaque_softc *hif_hdl,
 		for (i = 0; i < scn->ce_count; i++) {
 			napii = napid->napis[i];
 			if (napii) {
-				HIF_DBG("deRegistering LRO for ce_id %d NAPI callback for %d flush_cb %p, lro_data %p\n",
+				HIF_DBG("deRegistering LRO for ce_id %d NAPI callback for %d flush_cb %pK, lro_data %pK\n",
 					i, napii->id, napii->lro_flush_cb,
 					napii->lro_ctx);
 				napii->lro_flush_cb = NULL;
@@ -516,7 +516,7 @@ int hif_napi_event(struct hif_opaque_softc *hif_ctx, enum qca_napi_event event,
 		BLACKLIST_OFF_PENDING
 	     } blacklist_pending = BLACKLIST_NOT_PENDING;
 
-	NAPI_DEBUG("%s: -->(event=%d, aux=%p)", __func__, event, data);
+	NAPI_DEBUG("%s: -->(event=%d, aux=%pK)", __func__, event, data);
 
 	if ((napid->state & HIF_NAPI_INITED) == 0) {
 		NAPI_DEBUG("%s: got event when NAPI not initialized",

moetayuko/android_kernel_xiaomi_sdm660

hif/src/ce/ce_main.c
@@ -1461,7 +1461,7 @@ static inline void hif_ce_do_recv(struct hif_msg_callbacks *msg_callbacks,
 			rxCompletionHandler(msg_callbacks->Context,
 					netbuf, pipe_info->pipe_num);
 	} else {
-		HIF_ERROR("%s: Invalid Rx msg buf:%p nbytes:%d",
+		HIF_ERROR("%s: Invalid Rx msg buf:%pK nbytes:%d",
 				__func__, netbuf, nbytes);
 		qdf_nbuf_free(netbuf);
 	}
@@ -1565,7 +1565,7 @@ static int hif_completion_thread_startup(struct HIF_CE_state *hif_state)
 		attr = host_ce_config[pipe_num];
 		if (attr.src_nentries) {
 			/* pipe used to send to target */
-			HIF_INFO_MED("%s: pipe_num:%d pipe_info:0x%p",
+			HIF_INFO_MED("%s: pipe_num:%d pipe_info:0x%pK",
 					 __func__, pipe_num, pipe_info);
 			ce_send_cb_register(pipe_info->ce_hdl,
 					    hif_pci_ce_send_done, pipe_info,

hif/src/hif_napi.c
@@ -180,17 +180,17 @@ int hif_napi_create(struct hif_opaque_softc   *hif_ctx,
 
 		init_dummy_netdev(&(napii->netdev));
 
-		NAPI_DEBUG("adding napi=%p to netdev=%p (poll=%p, bdgt=%d)",
+		NAPI_DEBUG("adding napi=%pK to netdev=%pK (poll=%pK, bdgt=%d)",
 			   &(napii->napi), &(napii->netdev), poll, budget);
 		netif_napi_add(&(napii->netdev), &(napii->napi), poll, budget);
 
 		NAPI_DEBUG("after napi_add");
-		NAPI_DEBUG("napi=0x%p, netdev=0x%p",
+		NAPI_DEBUG("napi=0x%pK, netdev=0x%pK",
 			   &(napii->napi), &(napii->netdev));
-		NAPI_DEBUG("napi.dev_list.prev=0x%p, next=0x%p",
+		NAPI_DEBUG("napi.dev_list.prev=0x%pK, next=0x%pK",
 			   napii->napi.dev_list.prev,
 			   napii->napi.dev_list.next);
-		NAPI_DEBUG("dev.napi_list.prev=0x%p, next=0x%p",
+		NAPI_DEBUG("dev.napi_list.prev=0x%pK, next=0x%pK",
 			   napii->netdev.napi_list.prev,
 			   napii->netdev.napi_list.next);
 
@@ -286,10 +286,10 @@ int hif_napi_destroy(struct hif_opaque_softc *hif_ctx,
 		}
 		if (0 == rc) {
 			NAPI_DEBUG("before napi_del");
-			NAPI_DEBUG("napi.dlist.prv=0x%p, next=0x%p",
+			NAPI_DEBUG("napi.dlist.prv=0x%pK, next=0x%pK",
 				  napii->napi.dev_list.prev,
 				  napii->napi.dev_list.next);
-			NAPI_DEBUG("dev.napi_l.prv=0x%p, next=0x%p",
+			NAPI_DEBUG("dev.napi_l.prv=0x%pK, next=0x%pK",
 				   napii->netdev.napi_list.prev,
 				   napii->netdev.napi_list.next);
 
@@ -355,7 +355,7 @@ int hif_napi_lro_flush_cb_register(struct hif_opaque_softc *hif_hdl,
 				}
 				napii->lro_flush_cb = lro_flush_handler;
 				napii->lro_ctx = data;
-				HIF_ERROR("Registering LRO for ce_id %d NAPI callback for %d flush_cb %p, lro_data %p\n",
+				HIF_ERROR("Registering LRO for ce_id %d NAPI callback for %d flush_cb %pK, lro_data %pK\n",
 					i, napii->id, napii->lro_flush_cb,
 					napii->lro_ctx);
 				rc++;
@@ -389,7 +389,7 @@ void hif_napi_lro_flush_cb_deregister(struct hif_opaque_softc *hif_hdl,
 		for (i = 0; i < scn->ce_count; i++) {
 			napii = napid->napis[i];
 			if (napii) {
-				HIF_DBG("deRegistering LRO for ce_id %d NAPI callback for %d flush_cb %p, lro_data %p\n",
+				HIF_DBG("deRegistering LRO for ce_id %d NAPI callback for %d flush_cb %pK, lro_data %pK\n",
 					i, napii->id, napii->lro_flush_cb,
 					napii->lro_ctx);
 				napii->lro_flush_cb = NULL;
@@ -506,7 +506,7 @@ int hif_napi_event(struct hif_opaque_softc *hif_ctx, enum qca_napi_event event,
 		BLACKLIST_OFF_PENDING
 	     } blacklist_pending = BLACKLIST_NOT_PENDING;
 
-	NAPI_DEBUG("%s: -->(event=%d, aux=%p)", __func__, event, data);
+	NAPI_DEBUG("%s: -->(event=%d, aux=%pK)", __func__, event, data);
 
 	if ((napid->state & HIF_NAPI_INITED) == 0) {
 		NAPI_DEBUG("%s: got event when NAPI not initialized",